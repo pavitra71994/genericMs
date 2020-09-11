1bfb4cb5f751e505ebf53865aa7df06a6e3911bc
pavitra71994 <git@github.com:pavitra71994/my-app.git>
2020-09-11 16:46:01 +0530

    # WARNING: head commit changed in the meantime
    Initial Commit

---
+++ genericMs/src/main/java/com/example/genericMs/GenericMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.genericMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericMsApplication.class, args);
+	}
+
+}
---
+++ genericMs/src/test/java/com/example/genericMs/GenericMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genericMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
