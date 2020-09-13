b0e4385bcdb6d656a2fec92db474a98e960f0062
pavitra71994 <git@github.com:pavitra71994/my-app.git>
2020-09-13 23:35:23 +0530

    Defectc : org.hibernate.exception.GenericJDBCException

--- examMicroservice/src/main/java/com/example/examMicroservice/bean/AnswerBean.java
+++ examMicroservice/src/main/java/com/example/examMicroservice/bean/AnswerBean.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class AnswerBean {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.TABLE )
 	public int ansId;
 	public String ans;
 	public boolean isAnsTrue;
